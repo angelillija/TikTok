92a2bb6143266f194ee0d59d13426cff4f72d2d2
angel <105955582+angelillija@users.noreply.github.com>
2023-09-28 01:49:58 -0400

    Delete sid_separator.py

--- sid_separator.py
+++
@@ -1,7 +0,0 @@
-import re
-
-for session_id in open("./output/accounts.txt").read().splitlines():
-    session = re.search(r"(\w{32})", session_id)
-    if session:
-        open("./sessions.txt", "a").write(f"{session.group(0)}\n")
-        print(session.group(0))
