773391f54f9569dfa1abcdbae27e0949c8e44721
feigns <110311925+feigns@users.noreply.github.com>
2023-10-08 23:05:47 -0400

    improved
    ignored improving my own project for this

--- Captcha Solver/solver.py
+++ Captcha Solver/solver.py
@@ -1,15 +1,16 @@
 # github.com/angelillija
 
-import requests
 import random
-import base64
-import numpy
 import time
 import cv2
+from io import BufferedReader
+from numpy import uint8, frombuffer
+from httpx import Client as Session
+
 
 class TikTokCaptchaSolver:
-    def __init__(self, device_id: int, install_id: int):
-        self.session = requests.Session()
+    def __init__(self, device_id: int, install_id: int) -> None:
+        self.session = Session()
         self.base_url = "https://rc-verification-i18n.tiktokv.com"
         self.params = {
             "aid": "1233",
@@ -21,20 +22,35 @@ class TikTokCaptchaSolver:
         }
 
     @staticmethod
-    def process_image(string):
-        nparr = numpy.frombuffer(base64.b64decode(string), numpy.uint8)
+    def process_image(buffer: BufferedReader):
+        nparr = frombuffer(buffer, uint8)
         image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
         blurred = cv2.GaussianBlur(cv2.cvtColor(image, cv2.COLOR_BGR2GRAY), (3, 3), 0)
-        return cv2.addWeighted(cv2.convertScaleAbs(cv2.Sobel(blurred, cv2.CV_16S, 1, 0, ksize=3)), 0.5, cv2.convertScaleAbs(cv2.Sobel(blurred, cv2.CV_16S, 0, 1, ksize=3)), 0.5, 0)
+        return cv2.addWeighted(
+            cv2.convertScaleAbs(cv2.Sobel(blurred, cv2.CV_16S, 1, 0, ksize=3)),
+            0.5,
+            cv2.convertScaleAbs(cv2.Sobel(blurred, cv2.CV_16S, 0, 1, ksize=3)),
+            0.5,
+            0,
+        )
 
     def solve_captcha(self) -> dict:
-        captcha = self.session.get(f"{self.base_url}/captcha/get", params=self.params).json()
-        puzzle, piece = [self.process_image(base64.b64encode(self.session.get(captcha["data"]["question"][f"url{url}"]).content)) for url in [1, 2]]
+        captcha = self.session.get(
+            url=f"{self.base_url}/captcha/get", params=self.params
+        ).json()
+        puzzle, piece = [
+            self.process_image(
+                self.session.get(captcha["data"]["question"][f"url{url}"]).content
+            )
+            for url in [1, 2]
+        ]
 
         time.sleep(1)
 
         randlength = round(random.uniform(50, 100))
-        max_loc = cv2.minMaxLoc(cv2.matchTemplate(puzzle, piece, cv2.TM_CCOEFF_NORMED))[3][0]
+        max_loc = cv2.minMaxLoc(cv2.matchTemplate(puzzle, piece, cv2.TM_CCOEFF_NORMED))[
+            3
+        ][0]
 
         return self.session.post(
             url=f"{self.base_url}/captcha/verify",
@@ -45,11 +61,11 @@ class TikTokCaptchaSolver:
                 "mode": "slide",
                 "reply": [
                     {
-                        "relative_time": i * randlength,
-                        "x": round(max_loc / (randlength / (i + 1))),
+                        "relative_time": (i * randlength),
+                        "x": round(max_loc / (randlength / i)),
                         "y": captcha["data"]["question"]["tip_y"],
                     }
-                    for i in range(randlength)
-                ]
-            }   
+                    for i in range(1, randlength)
+                ],
+            },
         ).json()
--- Signing/Mobile/Other/xor.py
+++ Signing/Mobile/Other/xor.py
@@ -1,5 +1,6 @@
 def encrypt(string):
     return "".join([hex(ord(c) ^ 5)[2:] for c in string])
 
+
 # Add string to encrypt
 print(encrypt(""))
